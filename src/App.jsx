108cb6041b1bc40dee18d65c552e10be42b5ff22
Rockey Simmons <codenamerockey@gmail.com>
2019-08-15 13:16:34 -0700

    added a footer component and styled it in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavComponent from "./components/Navigation/NavComponent";
 import HeroComponent from "./components/Hero/HeroComponent";
 import ImageComponent from "./components/ImageComp/ImageComponent";
 import FeaturedComponent from "./components/Featured/FeaturedComponent";
+import Footer from "./components/Footer/Footer";
 import axios from "axios";
 import "./App.css";
 import { Grid } from "semantic-ui-react";
@@ -11,45 +12,48 @@ import SideBarComponent from "./components/Sidebar/SideBarComponent";
 function App() {
   const [nasaData, setNasaData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      .then(res => {
-        const nasaImg = res.data;
-        const nasaData = res.data;
-        console.log(nasaData);
-        setNasaData(nasaImg);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+  //     .then(res => {
+  //       const nasaImg = res.data;
+  //       const nasaData = res.data;
+  //       console.log(nasaData);
+  //       setNasaData(nasaImg);
+  //     });
+  // }, []);
   if (!nasaData) {
     return <h3>Loading...</h3>;
   } else {
     return (
-      <Grid celled>
-        <NavComponent />
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <ImageComponent img={nasaData.url} />
-            <ImageComponent img={nasaData.url} />
-          </Grid.Column>
+      <div>
+        <Grid celled>
+          <NavComponent />
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <ImageComponent img={nasaData.url} />
+              <ImageComponent img={nasaData.url} />
+            </Grid.Column>
 
-          <Grid.Column width={13}>
-            <HeroComponent img={nasaData.url} />
-          </Grid.Column>
-        </Grid.Row>
+            <Grid.Column width={13}>
+              <HeroComponent img={nasaData.url} />
+            </Grid.Column>
+          </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column width={10}>
-            <FeaturedComponent
-              date={nasaData.date}
-              explanation={nasaData.explanation}
-            />
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <SideBarComponent date={nasaData.date} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <FeaturedComponent
+                date={nasaData.date}
+                explanation={nasaData.explanation}
+              />
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <SideBarComponent date={nasaData.date} />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function footer() {
+  return (
+    <footerDiv>
+      <span>copyright &copy; 2019</span>
+    </footerDiv>
+  );
+}
+
+export default footer;
